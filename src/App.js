2b0a2ad371c652470c4add77480eb406ee73c22b
sebastian.meckovski <sebastian.meckovski@co-operativebank.co.uk>
2023-09-21 08:25:30 +0100

    amplify graphql database testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { createBlog } from "./graphql/mutations";
+import { Amplify, API } from "aws-amplify";
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 function App() {
+  async function addBlog() {
+    try {
+      await API.graphql({
+        query: createBlog,
+        variables: { input: { name: "hola againn", description: "ohh yaaa" } },
+      });
+    } catch (err) {
+      console.log("error creating blog:", err);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +33,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={addBlog}>add blog</button>
       </header>
     </div>
   );
