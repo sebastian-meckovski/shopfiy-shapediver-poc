4c645058982319ebe7ea8e317ecc78f7067b72b8
sebastian.meckovski <sebastian.meckovski@co-operativebank.co.uk>
2023-09-21 21:39:31 +0100

    crud operations tested with exceptions of update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,122 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { createBlog } from "./graphql/mutations";
+import { createBlog, deleteBlog } from "./graphql/mutations";
+import { listBlogs } from "./graphql/queries";
 import { Amplify, API } from "aws-amplify";
-
 import awsExports from "./aws-exports";
+import { useEffect, useState } from "react";
 Amplify.configure(awsExports);
 
+const initialData = {
+  name: "",
+  description: "",
+};
+
 function App() {
-  async function addBlog() {
+  const [blogList, setBlogList] = useState();
+  const [formData, setFormData] = useState(initialData);
+
+  async function addBlog(name, description) {
     try {
       await API.graphql({
         query: createBlog,
-        variables: { input: { name: "hola againn", description: "ohh yaaa" } },
+        variables: { input: { name: name, description: description } },
+      }).then((result) => {
+        setBlogList((prev) => {
+          return [...prev, result.data.createBlog];
+        });
       });
     } catch (err) {
       console.log("error creating blog:", err);
     }
   }
 
+  async function getAllBlogs() {
+    try {
+      await API.graphql({
+        query: listBlogs,
+      }).then((response) => {
+        setBlogList(response.data?.listBlogs?.items);
+      });
+    } catch {}
+  }
+  useEffect(() => {
+    getAllBlogs();
+  }, []);
+
+  async function removeBLog(id) {
+    try {
+      await API.graphql({
+        query: deleteBlog,
+        variables: {
+          input: { id: id },
+        },
+      }).then((response) => {
+        setBlogList((prev) => {
+          return prev.filter((x) => {
+            return x?.id !== response?.data?.deleteBlog?.id;
+          });
+        });
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    addBlog(formData.name, formData.description);
+    setFormData(initialData);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={addBlog}>add blog</button>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            required
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <input
+            required
+            type="text"
+            placeholder="Description"
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+
+        {blogList &&
+          blogList.map((x) => {
+            return (
+              <span key={x.id}>
+                {x.name} - {x.description}{" "}
+                <button
+                  onClick={() => {
+                    removeBLog(x.id);
+                  }}
+                >
+                  X
+                </button>
+              </span>
+            );
+          })}
       </header>
     </div>
   );
