81292ef3d95e8751ada5bfb692ebddf5d359e06b
sebastian.meckovski <sebastian.meckovski@co-operativebank.co.uk>
2023-09-24 17:12:48 +0100

    routing added and debugged

---
+++ src/routes/Root.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Root() {
+
+    
+  return (
+    <>
+      <p>hello this is root</p>
+      <nav>
+        <a href={`/AddNewPullUpBar`}>Add new pull up bar</a>
+      </nav>
+    </>
+  );
+}
