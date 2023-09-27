a346351db82d4edf3a282de17f6e5d94db16fe77
sebastian.meckovski <sebastian.meckovski@co-operativebank.co.uk>
2023-09-27 22:37:59 +0100

    file upload proof of concept investigated

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,14 +1,70 @@
-import React from "react";
+import { useState, useRef, useEffect } from "react";
 
-export default function Root() {
+function Root() {
+  const [files, setFiles] = useState([]);
+  const input = useRef();
 
-    
+  const changeHandler = (e) => {
+    setFiles((prevFiles) => {
+      return [...prevFiles, ...e.target.files];
+    });
+  };
+  
+  useEffect(() => {
+    const dataTransfer = new DataTransfer();
+    files.forEach((file) => {
+      dataTransfer.items.add(file);
+    });
+    input.current.files = dataTransfer.files;
+  }, [files]);
+
+  const clearInputValue = (index) => {
+    const dataTransfer = new DataTransfer();
+    setFiles((prevFiles) => {
+      const newFiles = [...prevFiles];
+      newFiles.splice(index, 1);
+      newFiles.forEach((file) => {
+        dataTransfer.items.add(file);
+      });
+      input.current.files = dataTransfer.files;
+      return newFiles;
+    });
+  };
+
   return (
     <>
-      <p>hello this is root</p>
-      <nav>
-        <a href={`/AddNewPullUpBar`}>Add new pull up bar</a>
-      </nav>
+      <form>
+        <p>
+          <label htmlFor="image">Browse images</label>
+          <input
+            ref={input}
+            type="file"
+            id="image"
+            accept=".png, .jpg, .jpeg"
+            multiple // Allow multiple file selection
+            onChange={changeHandler}
+          />
+        </p>
+        <p>
+          <input type="submit" label="Upload" />
+        </p>
+      </form>
+      {files.length > 0 ? (
+        <div>
+          {files.map((file, index) => (
+            <div key={index} style={{ display: "inline-block", margin: "5px" }}>
+              <img
+                src={URL.createObjectURL(file)}
+                alt={`preview-${index}`}
+                style={{ width: "150px", height: "150px", border: "1px solid" }}
+                onClick={() => clearInputValue(index)}
+              />
+            </div>
+          ))}
+        </div>
+      ) : null}
     </>
   );
 }
+
+export default Root;
