3ea9e961e69112985e7d9c035997354afff85816
sebastian.meckovski <sebastian.meckovski@co-operativebank.co.uk>
2023-10-01 11:55:24 +0100

    temporary token solution

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,27 +2,11 @@ import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import "./Home.scss";
 
 export default function Home() {
-  console.log(process.env);
-  const key =
-    process.env.REACT_APP_SECRETS !== undefined
-      ? JSON.parse(process.env.REACT_APP_SECRETS).googleMapsApiKey
-      : null;
+  const key = process.env.REACT_APP_GOOGLE_MAPS;
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: key,
   });
-
-  console.log('process.env.TEST_VARIABLE')
-  console.log(process.env.REACT_APP_TEST_VAR)
-
-  console.log('process.env.REACT_APP_SECRETS')
-  console.log(process.env.REACT_APP_SECRETS)
-
-  console.log('process.env');
-  console.log(process.env);
-
-  console.log('key:');
-  console.log(key);
 
   if (!isLoaded) return <div>loading...</div>;
 
