283d39331cb1567369c6e1dd08b5bdede451dbd9
sebastian-meckovski <sebastian.meckovski@gmail.com>
2024-10-27 23:06:22 +0100

    repurposed project for azure functions

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import Root from "./routes/Root";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./error-page";
-import Home from "./routes/Home";
+const { app } = require('@azure/functions');
 
-const router = createBrowserRouter([
-  {
-    path: "/TestingUpload",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/AddNewPullUpBar",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+app.setup({
+    enableHttpStream: true,
+});
