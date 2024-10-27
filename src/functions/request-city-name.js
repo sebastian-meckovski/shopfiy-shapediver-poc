4ba4450b702545dda01f4b64685a0f52d909b3cd
sebastian-meckovski <sebastian.meckovski@gmail.com>
2024-10-28 00:02:17 +0100

    triggering

--- src/functions/request-city-name.js
+++ src/functions/request-city-name.js
@@ -5,7 +5,6 @@ app.http('request-city-name', {
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
-
         const name = request.query.get('name') || await request.text() || 'world';
 
         return { body: `Hello, ${name}! This has been updated from pipeline` };
