218f37db16cce33adde01cea7be21b93bca5f0ec
Sebastian Meckovski <sebastian.meckovski@ef.com>
2025-04-08 21:41:07 +0200

    added shapediver as external libraries in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
     assetsInlineLimit: 0,
   },
   ssr: {
+    external: ['@shapediver/viewer.session', '@shapediver/viewer.viewport'],
     optimizeDeps: {
       /**
        * Include dependencies here if they throw CJS<>ESM errors.
@@ -43,7 +44,7 @@ export default defineConfig({
        * Include 'example-dep' in the array below.
        * @see https://vitejs.dev/config/dep-optimization-options
        */
-      include: ['@shapediver/viewer.session'],
+      include: [],
     },
   },
 });
